342b4e40cfabad4b95183b496be339ee36c5011e
afew3232 <afew3232@gmail.com>
2020-02-12 19:07:19 +0900

    before Rspec

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,8 @@
 class Book < ApplicationRecord
 
 	validates :title, presence: true
-	validates :body, presence: true
+	validates :body, presence: true, length:{maximum: 200}
 	belongs_to :user  #, optional: true #有効にすると、user_idがnilでもvalidationでない
-
 
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
 	devise :database_authenticatable, :registerable,
 	:recoverable, :rememberable, :validatable
 
-	validates :name, presence: true, length: {minimum: 2}
+	validates :name, presence: true, length: {minimum: 2, maximum: 20}
+	validates :introduction, length:{maximum: 50}
 
 	has_many :book, dependent: :destroy
 end
